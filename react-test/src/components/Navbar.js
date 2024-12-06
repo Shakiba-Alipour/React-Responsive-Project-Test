4dd2e47ca4c7900e62b64b32eb4ad1257fabc94d
Shakiba Alipour <alipour.sh79@gmail.com>
2024-12-07 00:54:46 +0330

    navbar is added

---
+++ react-test/src/components/Navbar.js
@@ -0,0 +1,104 @@
+import "./Navbar.css";
+import * as React from "react";
+import { useState, useEffect } from "react";
+import Link from "@mui/material/Link";
+import { ReactComponent as Logo } from "../assets/logo.svg";
+import { Button, ButtonGroup } from "@mui/material";
+
+const NavbarItems = [
+  {
+    key: "home",
+    label: "Home",
+    to: "home-section",
+  },
+  {
+    key: "service",
+    label: "Service",
+    to: "service-section",
+  },
+  {
+    key: "feature",
+    label: "Feature",
+    to: "feature-section",
+  },
+  {
+    key: "product",
+    label: "Poduct",
+    to: "product-section",
+  },
+  {
+    key: "testimonial",
+    label: "Testimonial",
+    to: "testimonial-section",
+  },
+  {
+    key: "faq",
+    label: "FAQ",
+    to: "faq-section",
+  },
+];
+
+export default function Navbar() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 780);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth <= 780);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <header id="navbar">
+      <Link href="/" id="logo-holder" underline="none">
+        <Logo id="logo" />
+        <h2 id="name">Nexcent</h2>
+      </Link>
+
+      {isMobile ? <MobileNavbarManagement /> : <DesktopNavbarManagement />}
+    </header>
+  );
+}
+
+function MobileNavbarManagement() {
+  //   return (
+  //     <div id="menu-holder">
+  //       <ul id="menu">
+  //         {NavbarItems.map((item) => (
+  //           <li className="nav-link" key={item.key}>
+  //             {item.label}
+  //           </li>
+  //         ))}
+  //       </ul>
+  //       <div id="sign-in-holder">
+  //         <Button id="login" variant="text">
+  //           Login
+  //         </Button>
+  //         <Button id="sign-up" variant="contained">
+  //           Sign up
+  //         </Button>
+  //       </div>
+  //     </div>
+  //   );
+}
+
+function DesktopNavbarManagement() {
+  return (
+    <div id="menu-holder">
+      <ul id="menu">
+        {NavbarItems.map((item) => (
+          <li className="nav-link" key={item.key}>
+            {item.label}
+          </li>
+        ))}
+      </ul>
+      <div id="sign-in-holder">
+        <Button id="login" variant="text">
+          Login
+        </Button>
+        <Button id="sign-up" variant="contained">
+          Sign up
+        </Button>
+      </div>
+    </div>
+  );
+}
