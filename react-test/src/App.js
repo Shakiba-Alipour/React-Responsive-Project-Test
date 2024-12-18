852234a2f659ddc5a66dad3253f6c9bb32aa0591
Shakiba Alipour <alipour.sh79@gmail.com>
2024-12-18 22:18:16 +0330

    all sections are added to the app

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import Home from "./components/Home.js";
 import Navbar from "./components/Navbar.js";
+import Clients from "./components/Clients.js";
+import Info from "./components/Info.js";
+import Demo from "./components/Demo.js";
+import Blog from "./components/Blog.js";
+import CustomFooter from "./components/Footer.js";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Clients />
+      <Info />
+      <Blog />
+      <Demo />
+      <CustomFooter />
     </div>
   );
 }
